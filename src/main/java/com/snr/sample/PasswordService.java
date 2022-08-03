ac2428021f5a3be069e9d01b16f2e676e3ed2189
Nagaraju <stambalanagaraju@99gmail.com>
2022-08-03 10:04:10 +0530

    Fist Commit

---
+++ src/main/java/com/snr/sample/PasswordService.java
@@ -0,0 +1,20 @@
+package com.snr.sample;
+
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Base64.Encoder;
+
+public class PasswordService {
+	
+	public static String encoded(String text) {
+		Encoder encoder = Base64.getEncoder();
+		return encoder.encodeToString(text.getBytes());
+	}
+	
+	public static String decoded(String encodeText) {
+		Decoder decorder = Base64.getDecoder();
+		byte[] decoder = decorder.decode(encodeText);
+		return new String(decoder);
+	}
+
+}
